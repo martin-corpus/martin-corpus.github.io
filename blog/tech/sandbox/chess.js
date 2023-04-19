df15e72be45fcba93f43035dd03996212018094e
martin corpus <nasuntao@yahoo.com>
2023-04-20 07:51:25 +1200

    Update Sandbox

---
+++ blog/tech/sandbox/chess.js
@@ -0,0 +1,13 @@
+
+// create board
+// make every other square green
+// create pieces and position - 2 dimensional array?
+// make logic - piece movement
+// make logic - checkmate / stalemate
+// make logic - capturing pieces
+// make logic - promoting peices
+// undo button
+//display captured peices
+//highlighting legal moves
+//show game history
+
